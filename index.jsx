0dd1dc7214e6acc78ca610b1877057fa6a3b8b05
fakesinging <manasrikhari23@gmail.com>
2025-01-19 23:33:31 +0530

    removed home line

--- index.jsx
+++ index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./vanapp/pages/Home"
 import About from "./vanapp/pages/About"
 import Vans from "./vanapp/pages/Vans/Vans"
 import { VanDetails } from './vanapp/pages/Vans/VanDetails';
